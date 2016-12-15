29634e9075c00382c9e3daafad08af21e4a50e6f
Jiayang Yan <raymond.dream@gmail.com>
2016-12-15 10:14:55 +0800

    long time no push. this time i upload a bright new function named 'like'

--- pages/search/search.js
+++ pages/search/search.js
@@ -1,5 +1,3 @@
-//index.js
-//获取应用实例
 var app = getApp();
 Page({
     data: {
@@ -9,7 +7,15 @@ Page({
         book_count: -1,
         loading: false,
         scroll_height: 0,
-        button_bcolor: "#b2b2b2"
+        button_bcolor: "#b2b2b2",
+        has_school: false,
+        choose_school: false,
+        school: "",
+        school_name: "",
+        provinces: [],
+        province_index: 0,
+        schools: [],
+        school_index: 0,
     },
     bindSearchInput: function (e) {
         this.setData({
@@ -27,60 +33,173 @@ Page({
             });
         }
     },
-    autoSearch: function () {
-        if (this.data.book_name === "") return;
+    searchBooks: function (e) {
         var that = this;
-        that.setData({loading: true});
+        if (that.data.book_name === "") return;
+        that.setData({ loading: true });
+        console.log(e.detail.value)
+        that.searchRequest(that.data.book_name)
+    },
+    searchRequest: function (book_name) {
+        var that = this
         wx.request({
             url: 'https://cornerapp.applinzi.com/api/search',
             method: "POST",
-            data: 'book=' + that.data.book_name,
+            data: 'book=' + book_name + '&school=' + that.data.school,
             header: {
                 'Content-Type': 'application/json'
             },
             success: function (res) {
-                that.setData({
-                    loading: false,
-                    books: res.data.books,
-                    is_alot: res.data.is_alot,
-                    book_count: res.data.book_count
-                });
+                if (res.statusCode == 500) {
+                    wx.showToast({
+                        title: '服务器连接错误',
+                        icon: 'loading',
+                        duration: 1500
+                    });
+                    that.setData({
+                        loading: false,
+                    });
+                } else if (res.statusCode == 200) {
+                    that.setData({
+                        loading: false,
+                        books: res.data.books,
+                        is_alot: res.data.is_alot,
+                        book_count: res.data.book_count
+                    });
+                }
             }
         })
     },
-    searchBooks: function () {
-        if (this.data.book_name === "") return;
+    changeSchoolRequest: function () {
         var that = this;
-        that.setData({loading: true});
-        wx.request({
-            url: 'https://cornerapp.applinzi.com/api/search',
-            method: "POST",
-            data: 'book=' + that.data.book_name,
-            header: {
-                'Content-Type': 'application/json'
-            },
+        that.setData({
+            provinces: app.provinces,
+            schools: app.schools[app.provinces[that.data.province_index]]
+        });
+        setTimeout(function () {
+            that.setData({
+                choose_school: true
+            });
+        }, 200);
+
+        wx.setNavigationBarTitle({ title: "选择学校" });
+    },
+    cancelChangeSchool: function () {
+        this.setData({
+            choose_school: false
+        });
+        wx.setNavigationBarTitle({ title: this.data.school_name + "图书馆查询" });
+    },
+    bindProvinceChange: function (e) {
+        this.setData({
+            province_index: e.detail.value,
+            schools: app.schools[app.provinces[e.detail.value]],
+            school_index: 0
+        })
+    },
+    bindSchoolChange: function (e) {
+        this.setData({
+            school_index: e.detail.value,
+        })
+    },
+    confirmChooseSchool: function () {
+        wx.setStorageSync('school', app.codes[this.data.schools[this.data.school_index]]);
+        wx.setStorageSync('school_name', this.data.schools[this.data.school_index]);
+        wx.setStorageSync('school_index', this.data.school_index);
+        wx.setStorageSync('province_index', this.data.province_index);
+        this.setData({
+            choose_school: false,
+            has_school: true,
+            school: wx.getStorageSync('school'),
+            school_name: wx.getStorageSync('school_name')
+        });
+        wx.setNavigationBarTitle({ title: this.data.school_name + "图书馆查询" });
+        this.setData({
+            book_name: "",
+            book_count: -1,
+            is_alot: false,
+            button_bcolor: "#b2b2b2"
+        });
+    },
+    onShow: function () {
+        var that = this;
+        wx.getStorage({
+            key: 'school',
             success: function (res) {
                 that.setData({
-                    loading: false,
-                    books: res.data.books,
-                    is_alot: res.data.is_alot,
-                    book_count: res.data.book_count
+                    has_school: true,
+                    choose_school: false,
+                    school: wx.getStorageSync('school'),
+                    school_name: wx.getStorageSync('school_name'),
+                })
+                wx.setNavigationBarTitle({ title: that.data.school_name + "图书馆查询" });
+            },
+            fail: function (res) {
+                that.setData({
+                    has_school: false,
+                    choose_school: true,
                 });
-            }
-        })
+                wx.setNavigationBarTitle({ title: "选择学校" });
+            },
+        });
     },
     onLoad: function () {
         var that = this;
-        app.getUserInfo(function (userInfo) {
-            that.setData({
-                userInfo: userInfo
-            });
-            that.update()
+        wx.getStorage({
+            key: 'school',
+            success: function (res) {
+                that.setData({
+                    has_school: true,
+                    choose_school: false,
+                    school: wx.getStorageSync('school'),
+                    school_name: wx.getStorageSync('school_name'),
+                })
+                wx.setNavigationBarTitle({ title: that.data.school_name + "图书馆查询" });
+            },
+            fail: function (res) {
+                that.setData({
+                    has_school: false,
+                    choose_school: true,
+                });
+                wx.setNavigationBarTitle({ title: "选择学校" });
+            },
         });
+        wx.getStorage({
+            key: 'school_index',
+            success: function (res) {
+                that.setData({
+                    school_index: res.data,
+                })
+            },
+            fail: function () {
+                that.setData({
+                    school_index: 0,
+                })
+            },
+        });
+        wx.getStorage({
+            key: 'province_index',
+            success: function (res) {
+                that.setData({
+                    province_index: res.data,
+                })
+            },
+            fail: function () {
+                that.setData({
+                    province_index: 0,
+                })
+            },
+        });
+        if (!that.data.has_school) {
+            that.setData({
+                provinces: app.provinces,
+                schools: app.schools[app.provinces[0]]
+            })
+        };
         wx.getSystemInfo({
             success: function (res) {
                 that.setData({
-                    scroll_height: res.windowHeight * 0.71
+                    scroll_height: res.windowHeight * 0.67
                 })
             }
         });
